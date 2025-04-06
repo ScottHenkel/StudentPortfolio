730c9832d2ee31526ce203aac18c7b1c6750830a
ScottHenkel <45054963+ScottHenkel@users.noreply.github.com>
2025-04-06 17:39:37 -0500

    Update Contact.aspx.cs

--- Contact.aspx.cs
+++ Contact.aspx.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -11,7 +11,10 @@ namespace StudentPortfolio
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            if (Session["Email"] == null)
+            {
+                Response.Redirect("Login.aspx");
+            }
         }
     }
 }
